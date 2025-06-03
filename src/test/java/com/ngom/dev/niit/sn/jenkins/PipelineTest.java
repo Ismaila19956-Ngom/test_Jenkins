125e3534673fcfc89fe42ccce4eec772d58de29e
Ismaila19956-Ngom <ngomismailadonde@gmail.com>
2025-06-03 11:47:42 +0200

    nouveaux ajout de methode pour tester a nouveau mon pipline

---
+++ src/test/java/com/ngom/dev/niit/sn/jenkins/PipelineTest.java
@@ -0,0 +1,11 @@
+package com.ngom.dev.niit.sn.jenkins;
+
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
+public class PipelineTest {
+    @Test
+    void testPipelineIsRunning() {
+        String message = "Pipeline is running!";
+        assertEquals("Pipeline is running!", message);
+    }
+}
